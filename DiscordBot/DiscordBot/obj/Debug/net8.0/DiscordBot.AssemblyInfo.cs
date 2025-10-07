7c8dbae7ec294f41a50a2f075dd51c019fc40ffc
Filip Lafata <f.lafata@kdkautomotive.com>
2025-10-07 13:40:34 +0200

    Update VS and build cache files for DiscordBot
    Updated Visual Studio workspace, document layout, and build cache files to reflect new file paths and recent changes. This includes changes to .vs, obj, and related configuration files, likely due to moving the project directory or opening new files in the IDE.

--- DiscordBot/DiscordBot/obj/Debug/net8.0/DiscordBot.AssemblyInfo.cs
+++ DiscordBot/DiscordBot/obj/Debug/net8.0/DiscordBot.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DiscordBot")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+835f3c5fa01b1cb642f1a6c12686d3f2671dead6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+712089a745019911911a427494eb46ec25748e41")]
 [assembly: System.Reflection.AssemblyProductAttribute("DiscordBot")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DiscordBot")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
